c318f19906bcf23999e60902bf93810fcea66e44
Techmad <techmad60@gmail.com>
2024-10-14 12:14:32 -0700

    well

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import "./App.css"
+import { FaMoon } from "react-icons/fa"
 export default function App () {
   return (
-    <>
-      <p className="text-red-500">Blessings and honor, glory and power</p> 
-    </>
+    <header>
+      <div className="flex items-center justify-between">
+        <div>
+          <h1>Where in the world?</h1>
+        </div>
+        <div className="flex items-center gap-2">
+          <FaMoon className="text-white stroke-black stroke-[10px]" />
+          <p>Light mode</p>
+        </div>
+      </div>
+      
+    </header>
   )
 }
